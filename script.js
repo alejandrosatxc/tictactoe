c6eb6967b8d2f29d96d015cf5e43614c06f9ab92
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-03 19:33:11 -0500

    Add event delegation to <ul>, breakout functions

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 var boardEl = $('#board')
-var game = 'over'
 var player;
 
 $('#start-btn').on("click", startGame)
 
-function startGame() {
+//clearBoard() will clear and reset the game board
+function clearBoard() {
+    var rows = boardEl.children('ul')
+    rows.off("click")
+    rows.children().text('')
+    rows.children().css("cursor", "none")
+}
 
-    //Clear game board
-    boardEl.children('ul').children().each(function () {
-        //Remove all remaining click handlers
-        $(this).off("click")
-        //Remove all X's and O's
-        $(this).text('')
-        //Remove cursor styling
-        $(this).css("cursor", "none")
-    })
+//coinflip() randomly sets 'player' to 0 or 1 after a one second countdown
+function coinflip() {
 
-    //Decide which player goes first randomly, like a coin flip, with a small countdown
     $('#status').text("Deciding who starts")
     var countDown = 10
     var coinFlipTimer = setInterval(function () {
@@ -32,24 +29,36 @@ function startGame() {
             clearInterval(coinFlipTimer)
         }
     }, 100)
+}
 
+//startGame() runs when the start button $('#start-btn') is clicked
+function startGame() {
 
-    //For each <li> in boardEl...
-    boardEl.children('ul').children().each(function () {
-        //Add a click event handler that will run game logic
-        $(this).on("click", function () {
-            //If player is 1, print X, else print O
-            player ? $(this).text('X') : $(this).text('O')
-            //Toggle player turn, if player = 1, set player = 0 and vise versa
-            player ? player = 0 : player = 1
-            $('#status').text(`Player ${player + 1}'s turn!`)
-            //Remove event listner so this square cannot be clicked again
-            $(this).off("click")
-            //Change cursor for this square to 'not-allowed'
-            $(this).css("cursor", "not-allowed")
-        })
-        //Change the cursor CSS for each square to 'pointer'
-        $(this).css("cursor", "pointer")
-    })
+    //Clear game board
+    clearBoard()
 
+    //Decide which player goes first randomly, like a coin flip, with a small countdown
+    coinflip()
+
+    //Add event handler (using delegation), to register clicks on the board and make a move. 
+    boardEl.children('ul').on('click', makeMove)
+
+    //Change the cursor CSS for each square to 'pointer'
+    boardEl.children('ul').children().css("cursor", "pointer")
+
+}
+
+function makeMove(e) {
+    //This function utilizes e.target (event delegation) to specify which box on the game board was clicked
+    //If square is empty (otherwise do nothing)
+    if ($(e.target).text() === '') {
+        //If player is 1, print X, else print O
+        player ? $(e.target).text('X') : $(e.target).text('O')
+        //Toggle player turn, if player = 1, set player = 0 and vise versa
+        player ? player = 0 : player = 1
+        //Remove event listner so this square cannot be clicked again
+        $('#status').text(`Player ${player + 1}'s turn!`)
+        //Change cursor for this square to 'not-allowed'
+        $(e.target).css("cursor", "not-allowed")
+    }
 }
